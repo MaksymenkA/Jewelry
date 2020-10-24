7c3072f362ee2e6c32b721b7a1105ccffe24e871
Alona <maksimenko.alona@gmail.com>
2020-10-24 18:09:40 +0200

    added images , created testimontials, what`s hot, banners, blog, insta follow, backgrounf video

--- main/main.js
+++ main/main.js
@@ -23,7 +23,31 @@
 //     $dropdown.off("mouseenter mouseleave");
 //   }
 // });
-
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    responsiveClass: true,
+    stagePadding: 23,
+    margin: 25,
+    loop: true,
+    responsive: {
+      150: {
+        items: 1,
+      },
+      250: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+      1200: {
+        items: 3,
+      }
+    }
+  });
+});
 $(document).ready(function (e) {
   $('.nav-toggle').click(function (event) {
     event.preventDefault();
