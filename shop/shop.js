649b7094663e952f929a79f204f581de59ae3275
Alona <maksimenko.alona@gmail.com>
2020-10-29 18:40:10 +0100

    added images, completed services. gallery, blog pages

--- shop/shop.js
+++ shop/shop.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
 
-    const $valueSpan = $('.valueSpan');
-    const $value = $('#slider11');
-    $valueSpan.html($value.val());
-    $value.on('input change', () => {
-  
-      $valueSpan.html($value.val());
-    });
   });
 
-  range.addEventListener("input", () => {
-    bubble.innerHTML = rangel.value;
-  });
