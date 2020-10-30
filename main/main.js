ac4d1427bee5c5a42de0d1db3799613e11f22e88
Alona <maksimenko.alona@gmail.com>
2020-10-30 14:59:09 +0100

    fixed menu items

--- main/main.js
+++ main/main.js
@@ -1,29 +1,32 @@
 
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    responsiveClass: true,
-    stagePadding: 23,
-    margin: 25,
-    loop: true,
-    responsive: {
-      150: {
-        items: 1,
-      },
-      250: {
-        items: 1,
-      },
-      600: {
-        items: 2,
-      },
-      1000: {
-        items: 3,
-      },
-      1200: {
-        items: 3,
+  (function ($) {
+    $(".owl-carousel").owlCarousel({
+      responsiveClass: true,
+      stagePadding: 23,
+      margin: 25,
+      loop: true,
+      responsive: {
+        150: {
+          items: 1,
+        },
+        250: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        1000: {
+          items: 3,
+        },
+        1200: {
+          items: 3,
+        }
       }
-    }
+    });
   });
 });
+
 $(document).ready(function (e) {
   $('.nav-toggle').click(function (event) {
     event.preventDefault();
@@ -33,28 +36,24 @@ $(document).ready(function (e) {
   });
 
   $('#home').hover(function () {
-    console.log("hi2");
     $('#shop').hide();
     $('#shop').removeClass('d-flex');
     $('#services').hide();
     $('#services').removeClass('d-flex');
   });
   $('#blog').hover(function () {
-    console.log("hi2");
     $('#shop').hide();
     $('#shop').removeClass('d-flex');
     $('#services').hide();
     $('#services').removeClass('d-flex');
   });
   $('#gallery').hover(function () {
-    console.log("hi2");
     $('#shop').hide();
     $('#shop').removeClass('d-flex');
     $('#services').hide();
     $('#services').removeClass('d-flex');
   });
   $('#shopDropdown').hover(function () {
-    console.log("hi");
     $('#shop').addClass('d-flex');
     $('#shop').hover(function () {
     }, function () {
@@ -62,10 +61,8 @@ $(document).ready(function (e) {
       $('#shop').removeClass('d-flex');
     })
   });
-  
 
   $('#servicesDropdown').hover(function () {
-    console.log("hi");
     $('#services').addClass('d-flex');
     $('#services').hover(function () {
     }, function () {
@@ -80,17 +77,17 @@ $(document).ready(function (e) {
 $(document).ready(function () {
   "use strict";
   $(".lightbox").click(function () {
-      var imgsrc = $(this).attr('src');
-      $("body").append("<div class='img-popup'><span class='close-lightbox'>&times;</span><img src='" + imgsrc + "'></div>");
-      $(".close-lightbox, .img-popup").click(function () {
-          $(".img-popup").fadeOut(500, function () {
-              $(this).remove();
-          }).addClass("lightboxfadeout");
-      });
+    var imgsrc = $(this).attr('src');
+    $("body").append("<div class='img-popup'><span class='close-lightbox'>&times;</span><img src='" + imgsrc + "'></div>");
+    $(".close-lightbox, .img-popup").click(function () {
+      $(".img-popup").fadeOut(500, function () {
+        $(this).remove();
+      }).addClass("lightboxfadeout");
+    });
 
   });
   $(".lightbox").click(function () {
-      $(".img-popup").fadeIn(500);
+    $(".img-popup").fadeIn(500);
   });
 
 });
